d7f625bc420ab4be82f20ae2bdf61ceba7e59760
Otavio Lemos <otaviolemos@gmail.com>
2020-09-16 14:44:49 -0300

    refactor: extract function to make many rolls

--- src/game.spec.ts
+++ src/game.spec.ts
@@ -8,16 +8,18 @@ beforeEach(() => {
 
 describe('Bowling game', () => {
   test('should score 0 for gutter game', () => {
-    for (let i = 0; i < 20; i++) {
-      g.roll(0)
-    }
+    rollMany(20, 0)
     expect(g.score).toBe(0)
   })
 
   test('should score 20 for all ones game', () => {
-    for (let i = 0; i < 20; i++) {
-      g.roll(1)
-    }
+    rollMany(20, 1)
     expect(g.score).toBe(20)
   })
+
+  function rollMany (n: number, pins: number): void {
+    for (let i = 0; i < n; i++) {
+      g.roll(pins)
+    }
+  }
 })
