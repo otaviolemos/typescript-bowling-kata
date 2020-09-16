583063368505378f375479d322af7c600820e62d
Otavio Lemos <otaviolemos@gmail.com>
2020-09-16 15:07:40 -0300

    refactor: rename variable

--- src/game.ts
+++ src/game.ts
@@ -4,14 +4,14 @@ export class Game {
 
   score (): number {
     let score = 0
-    let i = 0
+    let frameIndex = 0
     for (let frame = 0; frame < 10; frame++) {
-      if (this.rolls[i] + this.rolls[i + 1] === 10) { // spare
-        score += 10 + this.rolls[i + 2]
-        i += 2
+      if (this.rolls[frameIndex] + this.rolls[frameIndex + 1] === 10) { // spare
+        score += 10 + this.rolls[frameIndex + 2]
+        frameIndex += 2
       } else {
-        score += this.rolls[i] + this.rolls[i + 1]
-        i += 2
+        score += this.rolls[frameIndex] + this.rolls[frameIndex + 1]
+        frameIndex += 2
       }
     }
     return score
