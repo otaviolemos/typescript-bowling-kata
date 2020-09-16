27e9932ad1cfe1ef2dde45c1872ab7bbcf86821d
Otavio Lemos <otaviolemos@gmail.com>
2020-09-16 15:24:26 -0300

    refactor: improve game design with score helper methods

--- src/game.ts
+++ src/game.ts
@@ -7,19 +7,30 @@ export class Game {
     let frameIndex = 0
     for (let frame = 0; frame < 10; frame++) {
       if (this.rolls[frameIndex] === 10) { // strike
-        score += 10 +
-          this.rolls[frameIndex + 1] +
-          this.rolls[frameIndex + 2]
+        score += 10 + this.strikeBonus(frameIndex)
         frameIndex++
       } else if (this.isSpare(frameIndex)) {
-        score += 10 + this.rolls[frameIndex + 2]
+        score += 10 + this.spareBonus(frameIndex)
         frameIndex += 2
       } else {
-        score += this.rolls[frameIndex] + this.rolls[frameIndex + 1]
+        score += this.sumOfBallsInFrame(frameIndex)
         frameIndex += 2
       }
     }
     return score
+  }
+
+  private sumOfBallsInFrame (frameIndex: number): number {
+    return this.rolls[frameIndex] + this.rolls[frameIndex + 1]
+  }
+
+  private strikeBonus (frameIndex: number): number {
+    return this.rolls[frameIndex + 1] +
+           this.rolls[frameIndex + 2]
+  }
+
+  private spareBonus (frameIndex: number): number {
+    return this.rolls[frameIndex + 2]
   }
 
   private isSpare (frameIndex: number): boolean {
