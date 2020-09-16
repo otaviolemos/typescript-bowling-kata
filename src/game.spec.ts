ae913b411bab5761175c36e108915823d296c86f
Otavio Lemos <otaviolemos@gmail.com>
2020-09-16 14:55:52 -0300

    refactor: improve game design

--- src/game.spec.ts
+++ src/game.spec.ts
@@ -9,13 +9,20 @@ beforeEach(() => {
 describe('Bowling game', () => {
   test('should score 0 for gutter game', () => {
     rollMany(20, 0)
-    expect(g.score).toBe(0)
+    expect(g.score()).toBe(0)
   })
 
   test('should score 20 for all ones game', () => {
     rollMany(20, 1)
-    expect(g.score).toBe(20)
+    expect(g.score()).toBe(20)
   })
+
+  // test('should score 16 with a spare followed by a 3 ball', () => {
+  //   g.roll(5)
+  //   g.roll(5) // spare
+  //   g.roll(3)
+  //   expect(g.score).toBe(16)
+  // })
 
   function rollMany (n: number, pins: number): void {
     for (let i = 0; i < n; i++) {
--- src/game.ts
+++ src/game.ts
@@ -1,11 +1,16 @@
 export class Game {
-  private _score: number = 0
+  private rolls: number[] = Array(21).fill(0)
+  private currentRoll: number = 0
 
-  get score (): number {
-    return this._score
+  score (): number {
+    let score = 0
+    for (let i = 0; i < this.rolls.length; i++) {
+      score += this.rolls[i]
+    }
+    return score
   }
 
   roll (pins: number): void {
-    this._score += pins
+    this.rolls[this.currentRoll++] = pins
   }
 }
