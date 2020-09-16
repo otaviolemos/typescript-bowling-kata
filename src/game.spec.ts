42df087d67ca68f0e913bc81eb52748e5c657199
Otavio Lemos <otaviolemos@gmail.com>
2020-09-16 14:32:20 -0300

    test: gutter game

--- src/game.spec.ts
+++ src/game.spec.ts
@@ -1,5 +1,11 @@
+import { Game } from './game'
+
 describe('Bowling game', () => {
-  test('', () => {
-    expect(1).toBe(1)
+  test('should score 0 for gutter game', () => {
+    const g: Game = new Game()
+    for (let i = 0; i < 20; i++) {
+      g.roll(0)
+    }
+    expect(g.score()).toBe(0)
   })
 })
